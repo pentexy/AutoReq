3952c6b8e93f2de5f9018b940e03c3a8ef76f437
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-10-23 16:37:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    API_ID = int(os.getenv("API_ID", 0))
+    API_HASH = os.getenv("API_HASH", "")
+    OWNER_ID = int(os.getenv("OWNER_ID", 0))
+    
+    USERBOT_SESSION = os.getenv("USERBOT_SESSION", "")
+    
+    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+    DB_NAME = os.getenv("DB_NAME", "auto_req_bot")
+    
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+
+config = Config()
