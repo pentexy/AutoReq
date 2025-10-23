3baf68c64993b08d3d656db24a136d6a77bb49e0
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-10-23 17:22:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH", "")
+    # Bot
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
     OWNER_ID = int(os.getenv("OWNER_ID", 0))
     
-    USERBOT_SESSION = os.getenv("USERBOT_SESSION", "")
+    # Telethon Userbot
+    API_ID = int(os.getenv("API_ID", 0))
+    API_HASH = os.getenv("API_HASH")
+    SESSION_STRING = os.getenv("USERBOT_SESSION", "")
     
-    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-    DB_NAME = os.getenv("DB_NAME", "auto_req_bot")
+    # Database
+    MONGODB_URL = os.getenv("MONGODB_URL")
+    DB_NAME = "auto_req_bot"
     
+    # Logging
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
 
 config = Config()
